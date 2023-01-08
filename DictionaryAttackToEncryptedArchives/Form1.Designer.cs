c863a59942abaa22fbcfa58ca4506217198a3d59
storm999 <storm_999_@hotmail.com>
2023-01-09 00:08:14 +0300

    Add project files.

---
+++ DictionaryAttackToEncryptedArchives/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace DictionaryAttackToEncryptedArchives
+{
+    partial class DictionaryAttackToArchiveFiles
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.arcPathBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.counterLabel = new System.Windows.Forms.Label();
+            this.dicpathBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(258, 165);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(42, 15);
+            this.resultLabel.TabIndex = 0;
+            this.resultLabel.Text = "Result:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // arcPathBox
+            // 
+            this.arcPathBox.Location = new System.Drawing.Point(19, 62);
+            this.arcPathBox.Name = "arcPathBox";
+            this.arcPathBox.Size = new System.Drawing.Size(506, 23);
+            this.arcPathBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(285, 45);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Paste archive file path here:  \r\nSharpCompress:     Zip, GZip, BZip2, Tar, XZ, LZ" +
+    "ip, Rar\r\nSevenZipSharp: 7z, Zip, GZip, BZip2, Tar, Xz, ";
+            // 
+            // counterLabel
+            // 
+            this.counterLabel.AutoSize = true;
+            this.counterLabel.Location = new System.Drawing.Point(100, 165);
+            this.counterLabel.Name = "counterLabel";
+            this.counterLabel.Size = new System.Drawing.Size(96, 15);
+            this.counterLabel.TabIndex = 4;
+            this.counterLabel.Text = "Tried Passwords: ";
+            // 
+            // dicpathBox
+            // 
+            this.dicpathBox.Location = new System.Drawing.Point(19, 114);
+            this.dicpathBox.Name = "dicpathBox";
+            this.dicpathBox.Size = new System.Drawing.Size(506, 23);
+            this.dicpathBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Paste dictionary path here:";
+            // 
+            // DictionaryAttackToArchiveFiles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 202);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dicpathBox);
+            this.Controls.Add(this.counterLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.arcPathBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.resultLabel);
+            this.Name = "DictionaryAttackToArchiveFiles";
+            this.Text = "DictionaryAttackToArchiveFiles 2023";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label resultLabel;
+        private Button button1;
+        private TextBox arcPathBox;
+        private Label label2;
+        private Label counterLabel;
+        private TextBox dicpathBox;
+        private Label label3;
+    }
+}
---
+++ DictionaryAttackToEncryptedArchives/Form1.cs
@@ -0,0 +1,90 @@
+using SevenZip;
+using SharpCompress;
+using SharpCompress.Common;
+using SharpCompress.Compressors.ADC;
+using SharpCompress.Readers;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace DictionaryAttackToEncryptedArchives
+{
+    public partial class DictionaryAttackToArchiveFiles : Form
+    {
+        public DictionaryAttackToArchiveFiles()
+        {
+            InitializeComponent();
+        }
+         
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SevenZipBase.SetLibraryPath(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? Environment.CurrentDirectory, "7z.dll"));
+
+            string dictionaryPath = dicpathBox.Text;
+            string filePath = arcPathBox.Text;
+            string fileExtension = ""; //we will have to check if the file chosen is ok
+            fileExtension = Path.GetExtension(filePath);
+
+            bool isFound = false;
+            int counter = 0;
+            String pass;
+            using (Stream dicStream = File.OpenRead(dictionaryPath))
+            using (StreamReader dicStreamReader = new StreamReader(dicStream, Encoding.UTF8, true))
+            while ((pass = dicStreamReader.ReadLine()) != null)
+            {
+                ReaderOptions ops = new ReaderOptions();
+                ops.Password = pass;
+
+                if (fileExtension == ".zip" || fileExtension == ".GZip" || fileExtension == ".BZip2" ||
+                    fileExtension == ".Tar" || fileExtension == ".Rar" || fileExtension == "LZip" || fileExtension == "XZ")
+                {
+                    using (Stream archStream = File.OpenRead(filePath))
+                    using (IReader reader = ReaderFactory.Open(archStream, ops))
+                    {
+                        while (reader.MoveToNextEntry())
+                        {
+                            if (!reader.Entry.IsDirectory)
+                            {
+                                try
+                                {
+                                    reader.WriteEntryToDirectory(@".\", new ExtractionOptions()
+                                    {
+                                        ExtractFullPath = true,
+                                        Overwrite = true
+                                    });
+                                    isFound = true;
+                                    resultLabel.Text = "Found Pass: " + pass;
+                                }
+                                catch
+                                {
+                                    counter++;
+                                    counterLabel.Text = "Tried Passwords: " + counter.ToString();
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (fileExtension == ".7z")
+                {
+                    try
+                    {
+                    SevenZipExtractor extractor = new SevenZipExtractor(filePath, pass);
+                    extractor.ExtractArchive(@".\");
+
+                    isFound = true;
+                    resultLabel.Text = "Found Pass: " + pass;
+                    }
+                    catch
+                    {
+                        counter++;
+                        counterLabel.Text = "Tried Passwords: " + counter.ToString();
+                    }
+                }
+            }
+            if(!isFound)
+            {
+                resultLabel.Text = "Result: not found";
+            }
+        }
+    }
+}
